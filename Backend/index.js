86d5b7556539de4de7faa532253b1fff6b107c76
anamorales13 <maldonado.morales@alu.uhu.es>
2020-11-26 13:46:10 +0100

    cambios index

--- Backend/index.js
+++ Backend/index.js
@@ -7,15 +7,13 @@ var app=require('./app');
 const socketio = require('socket.io')
 const http = require('http')
 
-
-var port=3900;
-
-
+var port= process.env.PORT || 3900;//variable puerto. El que queremos utilizar
+var url= process.env.MONGO_DB;
 
 //variables de entorno locales
 require('dotenv').config({path: 'variables.env'});
 
-console.log(process.env.MONGO_URI);
+console.log(process.env.MONGO_DB);
  
 
 mongoose.set('useFindAndModify', false);
@@ -25,14 +23,14 @@ mongoose.Promise = global.Promise;
 
 //
 //mongoose.connect('mongodb+srv://anamorales13:vBac1UreWvszfgNe@plataforma.2cxua.mongodb.net/test?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true})
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/baseerasmus",{ useUnifiedTopology: true, useNewUrlParser: true})        
+mongoose.connect(url || "mongodb://localhost:27017/baseerasmus",{ useUnifiedTopology: true, useNewUrlParser: true})        
 .then(()=>{
             console.log('La conexion a la BD se ha realizado con exito');
 
-            /*app.listen(port, ()=> {
+            app.listen(port, ()=> {
                 console.log('servidor corriendo en http://localhost:'+port );
             
-            });*/
+            });
         });
 
     
